ce6be449d146cf7f2f982c228da6e4ff2d09aefa
Mark Cunningham <mcunningham0410@gmail.com>
2021-11-30 16:36:04 +0000

    initial changes

--- transformational_optics.py
+++ transformational_optics.py
@@ -4,3 +4,26 @@ from manim import *
 def transformation_function():
     return True
 
+class transformation_optic(Scene):
+    def construct(self):
+        grid = ComplexPlane()
+        self.add(grid)
+
+        self.play(Create(grid,run_time=3,lag_ratio = 0.1))
+
+        grid.prepare_for_nonlinear_transform()
+
+        self.play(
+            grid.animate.apply_function(
+                lambda p: p
+                + np.array(
+                    [
+                        1/(p[1]),
+                        1/(p[0]),
+                        0,
+                    ]
+                )
+            ),
+            run_time = 3
+        )
+        self.wait()
